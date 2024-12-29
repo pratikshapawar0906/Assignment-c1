ca8212945c727b3b8590fa473625062aa9592d62
Pratiksha Pawar <pratikshapawar920@gmail.com>
2024-12-29 20:25:25 +0530

    index.html

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import "react-toastify/dist/ReactToastify.css"; 
 
