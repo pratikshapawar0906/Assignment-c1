bb3dad84e8b76a184c7da16dc1023d344b782df8
Pratiksha Pawar <pratikshapawar920@gmail.com>
2025-01-02 14:04:45 +0530

    add

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: 'esnext',  // Ensure ESNext targets modern environments
+    outDir: 'dist',
   },
 });
-
